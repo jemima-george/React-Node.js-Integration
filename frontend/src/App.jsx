9bdc6b386ac61aa58684d7935e09bc1e6c5d954c
Jemima <jemima.a.george@gmail.com>
2026-08-26 17:14:04 +0400

    Style Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="main-section">
+      <h1 className="heading">Backend with Node.js and Frontend with React</h1>
+      <ul className="list-container">
+        <li className="list-item">Item</li>
+      </ul>
+    </main>
+  )
+}
+
+export default App
