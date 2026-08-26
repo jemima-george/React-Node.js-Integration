b56ff524a7f127f08ea8c31fb9c26ec64453c4ff
Jemima <jemima.a.george@gmail.com>
2026-08-26 18:34:39 +0400

    Backend JSON data

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,29 @@
 import express from "express"
 import cors from "cors"
 
+const app = express();
+
 // cors is a bridge between frontend and backend
 const corsOptions = {
-    origin: ["http://localhost:5173/"]
+    origin: ["http://localhost:5173"]
 }
 
 app.use(cors(corsOptions))
 
-const app = express();
+app.get('/', (req,res) =>{
+    res.json({
+        blogPost:[
+            {
+                title: "Journey to the end of the Earth",
+                content: "An exploration to discover the mysteries of the end of the Earth. "
+            }, 
+            {
+                title: "Understanding the Universe: A Guide",
+                content: "A simple explanation of the vastness and wonders of the universe"
+            }
+        ]
+    })
+})
 
 app.listen(8080, () => {
     console.log("Server started on port 8080")
